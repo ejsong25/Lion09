b5d7b0cc41fbbe3b014fcfa8c4cb51388613a2a8
jiyehyeon <blseptw@gmail.com>
2023-09-20 18:13:13 +0900

    Lion09

---
+++ Lion09/src/main/java/com/lion09/Test1.java
@@ -0,0 +1,11 @@
+package com.lion09;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("집에 가자아아아아앙");
+
+	}
+
+}
---
+++ Lion09/src/test/java/com/lion09/Lion09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lion09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

acc26b8d57c7d7b149880fd9f1e4236bd29b9031
jiyehyeon <blseptw@gmail.com>
2023-09-26 11:27:49 +0900

    컨트롤러 추가, 고객센터 완성

---
+++ Lion09/src/main/java/com/lion09/controller/LionController.java
@@ -0,0 +1,37 @@
+package com.lion09.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LionController {
+	
+	@GetMapping("/index")
+	public String main() {
+		return "index";
+	}
+	
+	@GetMapping("/faqs")
+	public String faqs() {
+		return "faqs";
+	}
+	
+	@GetMapping("/qna")
+	public String qna() {
+		return "qna";
+	}
+	
+	@GetMapping("/qna_ok")
+	public String qna_ok() {
+		return "qna_ok";
+	}
+	
+	@GetMapping("/list1")
+	public String list1() {
+		return "list1";
+	}
+	@GetMapping("/list2")
+	public String list2() {
+		return "list2";
+	}
+}

9390f6da7e141dcb70af8e2378edf5188bced2e4
Eunjung Song <ejsong25@gmail.com>
2023-10-05 10:07:42 +0900

    스프링 빈 환경설정 변경

--- Lion09/src/main/java/com/lion09/SpringConfig.java
+++ Lion09/src/main/java/com/lion09/SpringConfig.java
@@ -1,22 +1,22 @@
 package com.lion09;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.lion09.user.MemberJPARepository;
+//import com.lion09.user.MemberService;
 
-import com.lion09.user.UserJPARepository;
-import com.lion09.user.UserService;
-
-//스프링 빈 등록
-@Configuration
-public class SpringConfig {
-	private final UserJPARepository userJPARepository;
-	
-	public SpringConfig(UserJPARepository userJPARepository) {
-		this.userJPARepository = userJPARepository;
-	}
-	
-	@Bean
-	public UserService userService() {
-		return new UserService(userJPARepository);
-	}
-}
+////스프링 빈 등록
+//@Configuration
+//public class SpringConfig {
+//	private final MemberJPARepository memberJPARepository;
+//	
+//	public SpringConfig(MemberJPARepository memberJPARepository) {
+//		this.memberJPARepository = memberJPARepository;
+//	}
+//	
+//	@Bean
+//	public MemberService userService() {
+//		return new MemberService(memberJPARepository);
+//	}
+//}

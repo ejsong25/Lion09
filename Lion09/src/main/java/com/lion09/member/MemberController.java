ca54a5d3c139f14c2f7165b1bccbd7b60740a185
Eunjung Song <ejsong25@gmail.com>
2023-10-25 17:52:48 +0900

    회원가입 검증 firstCommit

--- Lion09/src/main/java/com/lion09/member/MemberController.java
+++ Lion09/src/main/java/com/lion09/member/MemberController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,8 +40,7 @@ public class MemberController {
 		
 		//두 개 패스워드 불일치 검증
 		if(!form.getUserPwd1().equals(form.getUserPwd2())) {
-			result.rejectValue("password2", "passwordIncorrect",
-					"2개 패스워드 값이 일치하지 않습니다.");
+			result.addError(new FieldError("form", "userPwd2", "2개 패스워드 값이 일치하지 않습니다."));
 			return "signup_form";
 		}
 		
@@ -57,7 +58,7 @@ public class MemberController {
 			
 		} catch (DataIntegrityViolationException e) {
 			e.printStackTrace();
-			result.rejectValue("signupFailed", "이미 등록된 사용자입니다");
+			result.addError(new ObjectError("form", "이미 등록된 사용자입니다"));
 			return "signup_form";
 			
 		}catch (Exception e) {

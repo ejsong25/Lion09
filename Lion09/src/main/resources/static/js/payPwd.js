0eaa2c73a08faf95b7b59675b5d85a9c7606ec2c
jiyehyeon <blseptw@gmail.com>
2023-10-29 16:49:39 +0900

    모달 오류, 충전 오류 수정

--- Lion09/src/main/resources/static/js/payPwd.js
+++ Lion09/src/main/resources/static/js/payPwd.js
@@ -25,8 +25,12 @@ $(function(){
         }, 0);
         if (totalLength === 6) {
             $("#btnCheck").prop("disabled", false);
+            $("#btnRegister").prop("disabled", false);
+            $("#btnChange").prop("disabled", false);
         } else {
             $("#btnCheck").prop("disabled", true);
+            $("#btnRegister").prop("disabled", true);
+            $("#btnChange").prop("disabled", true);
         }
     });
     
